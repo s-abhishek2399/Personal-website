59d6ee05558d3945e1e2d74030d057df08d8207a
Abhishek Singh <s.abhishek2399@gmail.com>
2021-01-14 17:38:25 +0530

    addded features

---
+++ index.js
@@ -0,0 +1,8 @@
+var navList = document.getElementById("nav-lists");
+function Show() {
+  navList.classList.add("_Menus-show");
+}
+
+function Hide() {
+  navList.classList.remove("_Menus-show");
+}
