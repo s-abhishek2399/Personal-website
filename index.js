9d484c0f317f4779c5359d5f39a5a8bc7a0b3754
Abhishek Singh <s.abhishek2399@gmail.com>
2021-01-22 14:12:38 +0530

    project section added

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ function Show() {
 function Hide() {
   navList.classList.remove("_Menus-show");
 }
+
+//project
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+
+function scrll() {
+  var body = document.getElementsByClassName("warper");
+  body[0].scrollLeft += 90;
+}
+
+function scrlll() {
+  var body = document.getElementsByClassName("warper");
+  body[0].scrollLeft -= 90;
+}
+
+right.addEventListener("click", scrll);
+left.addEventListener("click", scrlll);
+//project end
